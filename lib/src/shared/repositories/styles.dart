1fb7f65beb90ee11c9a6677b41ab64d9390b0c95
1032445 <weverson.lima@novomundo.com.br>
2021-12-07 09:07:59 -0300

    Tarefa: declaração de estilos

---
+++ lib/src/shared/repositories/styles.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = const Color.fromRGBO(4, 4, 145, 1);
+Color secundaryColor = const Color.fromRGBO(0, 207, 128, 0.8);
+Color backgroundColor = const Color.fromRGBO(195, 184, 184, 0.10);
