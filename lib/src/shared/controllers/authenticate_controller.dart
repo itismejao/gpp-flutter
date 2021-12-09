77a224a1e7590a078690a61b21bd09b9dc4d7e6d
1032445 <weverson.lima@novomundo.com.br>
2021-12-09 09:59:23 -0300

    Tarefa: implementado o controle de autenticação

---
+++ lib/src/shared/controllers/authenticate_controller.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:gpp/src/shared/exceptions/user_exception.dart';
+import 'package:gpp/src/shared/models/token_model.dart';
+import 'package:gpp/src/shared/models/authenticate_model.dart';
+import 'package:gpp/src/shared/services/api.dart';
+
+class AuthenticateController {
+  Future<AuthenticateModel> login(uid, password) async {
+    try {
+      var response =
+          await api.post('/user/login', {'uid': uid, 'password': password});
+
+      if (response.statusCode == 500) {
+        throw UserNotFoundException();
+      }
+
+      return AuthenticateModel.fromJson(jsonDecode(response.body));
+    } on UserNotFoundException {
+      rethrow;
+    }
+  }
+
+  Future<TokenModel> createToken(email, password) async {
+    var response =
+        await api.post('/user/token', {'email': email, 'password': password});
+
+    try {
+      if (response.statusCode == 401) {
+        throw ("Usuário não possui token disponível");
+      }
+      return TokenModel.fromJson(jsonDecode(response.body));
+    } catch (error) {
+      rethrow;
+    }
+  }
+}
