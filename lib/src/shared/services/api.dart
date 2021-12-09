6985016d8cdacb79c7463af014da5e8e8a647a1c
1032445 <weverson.lima@novomundo.com.br>
2021-12-09 09:51:09 -0300

    Tarefa: adicionado o token no header das requisições

--- lib/src/shared/services/api.dart
+++ lib/src/shared/services/api.dart
@@ -1,19 +1,30 @@
 import 'dart:convert';
 
+import 'package:gpp/src/shared/services/auth.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Api {
   String? baseUrl = dotenv.env['API_URL'];
-  final Map<String, String> _headers = {
+  final Map<String, String> headers = {
     'Content-type': 'application/json',
-    //'Accept': 'application/json',
-    //'Authorization': 'Bearer $token'
+    'Accept': 'application/json',
   };
+
+  Map<String, String> getHeader() {
+    if (isAuthenticated()) {
+      String token = getToken();
+      headers.addAll({'Authorization': 'Bearer ' + token});
+    } else {
+      logout();
+    }
+
+    return headers;
+  }
 
   Future<dynamic> get(String endpoint) async {
     var uri = Uri.parse(baseUrl! + endpoint);
-    var response = await http.get(uri, headers: _headers);
+    var response = await http.get(uri, headers: getHeader());
 
     return response.body;
   }
@@ -21,9 +32,9 @@ class Api {
   Future<dynamic> post(String endpoint, body) async {
     var uri = Uri.parse(baseUrl! + endpoint);
     var response =
-        await http.post(uri, headers: _headers, body: jsonEncode(body));
+        await http.post(uri, headers: getHeader(), body: jsonEncode(body));
 
-    return response.body;
+    return response;
   }
 }
 
