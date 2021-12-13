7677f4a9ef7cd7481b38b5552d5862e2dd5e2479
1032445 <weverson.lima@novomundo.com.br>
2021-12-13 17:50:17 -0300

    Tarefa: adicionado o método textStyle

--- lib/src/shared/repositories/styles.dart
+++ lib/src/shared/repositories/styles.dart
@@ -3,3 +3,9 @@ import 'package:flutter/material.dart';
 Color primaryColor = const Color.fromRGBO(4, 4, 145, 1);
 Color secundaryColor = const Color.fromRGBO(0, 207, 128, 0.8);
 Color backgroundColor = const Color.fromRGBO(195, 184, 184, 0.10);
+
+TextStyle textStyle(
+    {Color? color, double? fontSize, FontWeight? fontWeight, double? height}) {
+  return TextStyle(
+      color: color, fontSize: fontSize, fontWeight: fontWeight, height: height);
+}
