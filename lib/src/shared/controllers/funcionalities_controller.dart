affc99b0fec823dc09c60b0a603b775d81b6ddef
1032445 <weverson.lima@novomundo.com.br>
2021-12-16 17:30:53 -0300

    Tarefa: implementado o controlador de funcionalidades

--- lib/src/shared/controllers/funcionalities_controller.dart
+++ lib/src/shared/controllers/funcionalities_controller.dart
@@ -1,16 +1,31 @@
 import 'dart:convert';
 
+import 'package:gpp/src/shared/exceptions/funcionalities_exception.dart';
 import 'package:gpp/src/shared/models/funcionalitie_model.dart';
-import 'package:gpp/src/shared/services/api.dart';
+import 'package:gpp/src/shared/repositories/status_code.dart';
+import 'package:gpp/src/shared/services/gpp_api.dart';
+import 'package:http/http.dart';
 
 class FuncionalitiesController {
   Future<List<FuncionalitieModel>> getFuncionalities(int id) async {
-    var response = await api.get('/user/funcionalidades/' + id.toString());
+    try {
+      Response response =
+          await gppApi.get('/user/funcionalidades/' + id.toString());
 
-    List<FuncionalitieModel> funcionalidades = jsonDecode(response.body)
-        .map<FuncionalitieModel>((data) => FuncionalitieModel.fromJson(data))
-        .toList();
+      if (response.statusCode == StatusCode.NOT_FOUND) {
+        var error = jsonDecode(response.body)['error'];
+        throw FuncionalitiesException(error);
+      }
 
-    return funcionalidades;
+      var data = jsonDecode(response.body);
+
+      List<FuncionalitieModel> funcionalidades = data.first
+          .map<FuncionalitieModel>((data) => FuncionalitieModel.fromJson(data))
+          .toList();
+
+      return funcionalidades;
+    } on FuncionalitiesException {
+      rethrow;
+    }
   }
 }
