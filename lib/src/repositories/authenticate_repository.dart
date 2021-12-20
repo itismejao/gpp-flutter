fb92e8a41a7aa46e55c9b35d681d741fa6c71786
1032445 <weverson.lima@novomundo.com.br>
2021-12-20 17:14:21 -0300

    Tarefa: criado o repositorio de autenticação

---
+++ lib/src/repositories/authenticate_repository.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:gpp/src/models/user_models.dart';
+import 'package:gpp/src/shared/exceptions/authenticate_exception.dart';
+import 'package:gpp/src/shared/models/authenticate_model.dart';
+import 'package:gpp/src/shared/repositories/global.dart';
+import 'package:gpp/src/shared/repositories/status_code.dart';
+import 'package:gpp/src/shared/services/auth.dart';
+import 'package:gpp/src/shared/services/gpp_api.dart';
+
+class AuthenticateRepository {
+  Future<bool> doLogin(UserModel user) async {
+    try {
+      var response = await gppApi.post('/user/login', user.toJson());
+
+      if (response.statusCode == StatusCode.UNAUTHORIZED) {
+        var error = jsonDecode(response.body)['error'];
+        throw AuthenticationException(error);
+      }
+
+      if (response.statusCode == StatusCode.OK) {
+        AuthenticateModel authenticate =
+            AuthenticateModel.fromJson(jsonDecode(response.body));
+
+        //Seta token
+        setToken(authenticate.accessToken);
+
+        authenticateUser = authenticate;
+        return true;
+      }
+
+      return false;
+    } on AuthenticationException {
+      rethrow;
+    }
+  }
+}
