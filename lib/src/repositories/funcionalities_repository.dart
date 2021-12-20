4a2bb8e96d8df02265fcf966de1581e97d9ab8f9
1032445 <weverson.lima@novomundo.com.br>
2021-12-20 17:14:33 -0300

    Tarefa: criado o repositorio de funcionalidades

---
+++ lib/src/repositories/funcionalities_repository.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:gpp/src/models/user_models.dart';
+import 'package:gpp/src/shared/exceptions/funcionalities_exception.dart';
+import 'package:gpp/src/models/funcionalitie_model.dart';
+import 'package:gpp/src/shared/repositories/status_code.dart';
+import 'package:gpp/src/shared/services/gpp_api.dart';
+import 'package:http/http.dart';
+
+class FuncionalitiesRepository {
+  Future<List<FuncionalitieModel>> fetchFuncionalities(UserModel user) async {
+    try {
+      Response response =
+          await gppApi.get('/user/funcionalidades/' + user.uid!);
+
+      if (response.statusCode == StatusCode.NOT_FOUND) {
+        var error = jsonDecode(response.body)['error'];
+        throw FuncionalitiesException(error);
+      }
+
+      var data = jsonDecode(response.body);
+
+      List<FuncionalitieModel> funcionalidades = data.first
+          .map<FuncionalitieModel>((data) => FuncionalitieModel.fromJson(data))
+          .toList();
+
+      return funcionalidades;
+    } on FuncionalitiesException {
+      rethrow;
+    }
+  }
+}
