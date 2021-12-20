c00e8ab2a957af8660447cd97f87e6b2f1e1e44c
1032445 <weverson.lima@novomundo.com.br>
2021-12-20 17:11:52 -0300

    Tarefa: criado a model de funcionalidades

---
+++ lib/src/models/funcionalitie_model.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class FuncionalitieModel {
+  String id;
+  String name;
+  String icon;
+  List<SubFuncionalities> subFuncionalities;
+  bool isExpanded = false;
+
+  FuncionalitieModel(
+      {required this.id,
+      required this.name,
+      required this.icon,
+      required this.subFuncionalities});
+
+  factory FuncionalitieModel.fromJson(Map<String, dynamic> json) {
+    var id = json['id'];
+    var name = json['name'];
+    var icon = json['icon'];
+    List<SubFuncionalities> subFuncionalities = [];
+    if (json['subFuncionalities'] != null) {
+      json['subFuncionalities'].forEach((data) {
+        subFuncionalities.add(SubFuncionalities.fromJson(data));
+      });
+    }
+    return FuncionalitieModel(
+        id: id, name: name, icon: icon, subFuncionalities: subFuncionalities);
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['icon'] = icon;
+    data['subFuncionalities'] =
+        subFuncionalities.map((v) => v.toJson()).toList();
+    return data;
+  }
+}
+
+class SubFuncionalities {
+  String id;
+  String name;
+  String icon;
+  String route;
+  Color? colorButton;
+  Border? border =
+      Border(left: BorderSide(color: Colors.grey.shade200, width: 2));
+
+  SubFuncionalities(
+      {required this.id,
+      required this.name,
+      required this.icon,
+      required this.route});
+
+  factory SubFuncionalities.fromJson(Map<String, dynamic> json) {
+    return SubFuncionalities(
+        id: json['id'],
+        name: json['name'],
+        icon: json['icon'],
+        route: json['route']);
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['icon'] = icon;
+    data['route'] = route;
+    return data;
+  }
+}
