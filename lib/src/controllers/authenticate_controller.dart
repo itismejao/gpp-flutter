da3fb951ff5ce067842ea965162390e6c1c3b0c1
1032445 <weverson.lima@novomundo.com.br>
2021-12-22 15:32:35 -0300

    Tarefa: alterado controlador de autenticação

--- lib/src/controllers/authenticate_controller.dart
+++ lib/src/controllers/authenticate_controller.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:gpp/src/models/user_models.dart';
+import 'package:gpp/src/models/user_model.dart';
 import 'package:gpp/src/repositories/authenticate_repository.dart';
 import 'package:gpp/src/shared/enumeration/authenticate_enum.dart';
 
