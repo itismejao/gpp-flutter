d93bdfbf3f234b552c3f683ec71665e5150e5e23
1032445 <weverson.lima@novomundo.com.br>
2021-12-22 15:39:50 -0300

    Tarefa: alterado nome do serviço da api

--- lib/src/shared/services/gpp_api.dart
+++ lib/src/shared/services/gpp_api.dart
@@ -4,8 +4,9 @@ import 'dart:convert';
 import 'package:gpp/src/shared/services/auth.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart';
 
-class GppApi {
+class ApiService {
   String? baseUrl = dotenv.env['API_URL'];
   Map<String, String> headers = {
     'Content-type': 'application/json',
@@ -27,20 +28,18 @@ class GppApi {
     return headers;
   }
 
-  Future<dynamic> get(String endpoint) async {
+  Future<Response> get(String endpoint) async {
     // ignore: avoid_print
 
     try {
       var uri = Uri.parse(baseUrl! + endpoint);
-      var response = await http
+      Response response = await http
           .get(uri, headers: getHeader())
           .timeout(const Duration(minutes: 1));
 
       return response;
-    } on TimeoutException catch (aa) {
-      // ignore: avoid_print
-      print(aa.message);
-      // throw TimeoutException("Erro de conexão com a API");
+    } on TimeoutException {
+      throw TimeoutException("Erro de conexão com a API");
     }
   }
 
@@ -66,4 +65,4 @@ class GppApi {
   }
 }
 
-final gppApi = GppApi();
+final gppApi = ApiService();
