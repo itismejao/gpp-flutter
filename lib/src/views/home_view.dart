1708acafc5719edd1be40dc63869206852344825
1032445 <weverson.lima@novomundo.com.br>
2021-12-27 17:24:18 -0300

    Tarefa: alterado visão home

--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gpp/src/shared/repositories/styles.dart';
+import 'package:gpp/src/views/app_bar.dart';
 import 'package:gpp/src/views/funcionalities_view.dart';
+import 'package:gpp/src/views/user_view.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
 class HomeView extends StatefulWidget {
@@ -13,6 +15,7 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
     return ResponsiveBuilder(
       builder: (context, sizingInformation) {
         if (sizingInformation.deviceScreenType == DeviceScreenType.mobile) {
@@ -31,16 +34,64 @@ class _HomeViewState extends State<HomeView> {
           );
         }
 
-        return Row(
-          children: [
-            const Expanded(child: FuncionalitiesView()),
-            Expanded(
-                flex: 4,
-                child: Container(
-                  color: backgroundColor,
-                ))
-          ],
+        return Scaffold(
+          backgroundColor: Colors.grey.shade200,
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(80.0),
+            child: AppBarView(),
+          ),
+          body: Column(
+            children: [
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: SizedBox(
+                            height: mediaQuery.size.height * 0.90,
+                            child: FuncionalitiesView()),
+                      ),
+                      Expanded(
+                        flex: 4,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Container(
+                            child: SizedBox(
+                                height: mediaQuery.size.height * 0.85,
+                                child: UserView()),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
         );
+
+        // return Scaffold(
+        //   body: Row(
+        //     children: [
+        //       const Expanded(child: FuncionalitiesView()),
+        //       Expanded(
+        //           flex: 4,
+        //           child: Container(
+        //             color: backgroundColor,
+        //             child: Column(
+        //               children: [
+        //                 Container(
+        //                   child: Row(
+        //                     children: [Text('ss')],
+        //                   ),
+        //                 )
+        //               ],
+        //             ),
+        //           ))
+        //     ],
+        //   ),
+        // );
       },
     );
   }
