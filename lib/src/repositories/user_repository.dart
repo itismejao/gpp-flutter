bfa68f3ac9bf30741b3bf66c69df36a1647b0756
1032445 <weverson.lima@novomundo.com.br>
2021-12-28 17:56:21 -0300

    Tarefa: alterado repositório de usuário

--- lib/src/repositories/user_repository.dart
+++ lib/src/repositories/user_repository.dart
@@ -4,7 +4,6 @@ import 'package:http/http.dart';
 
 import 'package:gpp/src/models/funcionalitie_model.dart';
 import 'package:gpp/src/models/user_model.dart';
-import 'package:gpp/src/shared/exceptions/funcionalities_exception.dart';
 import 'package:gpp/src/shared/exceptions/user_exception.dart';
 import 'package:gpp/src/shared/repositories/status_code.dart';
 import 'package:gpp/src/shared/services/gpp_api.dart';
@@ -55,7 +54,6 @@ class UserRepository {
         .map((subFuncionalitie) => subFuncionalitie.toJson())
         .toList();
 
-    print(jsonEncode(dataSend));
     Response response = await api.put(
         '/user/itensfuncionalidades/' + user.uid.toString(), dataSend);
 
