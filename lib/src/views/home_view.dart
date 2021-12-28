5ffcda437a19a5f15f3bd71ff095219cf489d365
1032445 <weverson.lima@novomundo.com.br>
2021-12-28 17:58:52 -0300

    Tarefa: alterado visão home

--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:gpp/src/shared/repositories/styles.dart';
-import 'package:gpp/src/views/app_bar.dart';
+import 'package:gpp/src/controllers/responsive_controller.dart';
+import 'package:gpp/src/models/user_model.dart';
+import 'package:gpp/src/views/appbar_view.dart';
 import 'package:gpp/src/views/funcionalities_view.dart';
 import 'package:gpp/src/views/user_view.dart';
-import 'package:responsive_builder/responsive_builder.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -13,86 +13,118 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final ResponsiveController _responsive = ResponsiveController();
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
-    return ResponsiveBuilder(
-      builder: (context, sizingInformation) {
-        if (sizingInformation.deviceScreenType == DeviceScreenType.mobile) {
-          return SafeArea(
-            child: Scaffold(
-              appBar: AppBar(
-                  backgroundColor: primaryColor, title: const Text('GPP')),
-              drawer: const Drawer(
-                  child:
-                      FuncionalitiesView() // Populate the Drawer in the next step.
-                  ),
-              body: Container(
-                color: backgroundColor,
-              ),
-            ),
-          );
-        }
 
+    Widget page = LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      if (_responsive.isMobile(constraints.maxWidth)) {
         return Scaffold(
           backgroundColor: Colors.grey.shade200,
           appBar: PreferredSize(
-            preferredSize: const Size.fromHeight(80.0),
+            preferredSize: Size.fromHeight(mediaQuery.size.height * 0.10),
             child: AppBarView(),
           ),
-          body: Column(
-            children: [
-              Column(
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        flex: 1,
-                        child: SizedBox(
-                            height: mediaQuery.size.height * 0.90,
-                            child: FuncionalitiesView()),
-                      ),
-                      Expanded(
-                        flex: 4,
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Container(
-                            child: SizedBox(
-                                height: mediaQuery.size.height * 0.85,
-                                child: UserView()),
-                          ),
+          drawer: Container(
+              color: Colors.white,
+              width: mediaQuery.size.width * 0.70,
+              child: const FuncionalitiesView()),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        color: Colors.white,
+                        height: 1000,
+                        child: Navigator(
+                          onGenerateRoute: (settings) {
+                            var route = settings.name;
+                            Widget page;
+                            switch (route) {
+                              case '/users':
+                                page = const UserView();
+                                break;
+                              case '/user_detail':
+                                final user = settings.arguments as UserModel;
+
+                                page = UserDetailView(user: user);
+                                break;
+                              default:
+                                page = const UserView();
+                            }
+
+                            return MaterialPageRoute(
+                                builder: (context) => page, settings: settings);
+                          },
                         ),
                       ),
-                    ],
-                  ),
-                ],
-              )
-            ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         );
+      }
 
-        // return Scaffold(
-        //   body: Row(
-        //     children: [
-        //       const Expanded(child: FuncionalitiesView()),
-        //       Expanded(
-        //           flex: 4,
-        //           child: Container(
-        //             color: backgroundColor,
-        //             child: Column(
-        //               children: [
-        //                 Container(
-        //                   child: Row(
-        //                     children: [Text('ss')],
-        //                   ),
-        //                 )
-        //               ],
-        //             ),
-        //           ))
-        //     ],
-        //   ),
-        // );
-      },
-    );
+      return Scaffold(
+        backgroundColor: Colors.grey.shade200,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(mediaQuery.size.height * 0.10),
+          child: AppBarView(),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                        color: Colors.white,
+                        height: 1000,
+                        child: const FuncionalitiesView()),
+                  ),
+                  Expanded(
+                    flex: 4,
+                    child: Container(
+                      color: Colors.white,
+                      height: 1000,
+                      child: Navigator(
+                        onGenerateRoute: (settings) {
+                          var route = settings.name;
+                          Widget page;
+                          switch (route) {
+                            case '/users':
+                              page = const UserView();
+                              break;
+                            case '/user_detail':
+                              final user = settings.arguments as UserModel;
+
+                              page = UserDetailView(user: user);
+                              break;
+                            default:
+                              page = const UserView();
+                          }
+
+                          return MaterialPageRoute(
+                              builder: (context) => page, settings: settings);
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    });
+
+    return page;
   }
 }
