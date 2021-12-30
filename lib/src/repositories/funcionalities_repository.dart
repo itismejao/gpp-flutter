17f7c4b5983acbad712b878db2d383a497314381
1032445 <weverson.lima@novomundo.com.br>
2021-12-30 17:29:58 -0300

    Tarefa: alterado repositório de funcionalidades

--- lib/src/repositories/funcionalities_repository.dart
+++ lib/src/repositories/funcionalities_repository.dart
@@ -1,32 +1,37 @@
-import 'dart:convert';
+// import 'dart:convert';
 
-import 'package:gpp/src/models/user_model.dart';
-import 'package:gpp/src/shared/exceptions/funcionalities_exception.dart';
-import 'package:gpp/src/models/funcionalitie_model.dart';
-import 'package:gpp/src/shared/repositories/status_code.dart';
-import 'package:gpp/src/shared/services/gpp_api.dart';
-import 'package:http/http.dart';
+// import 'package:gpp/src/models/user_model.dart';
+// import 'package:gpp/src/shared/exceptions/funcionalities_exception.dart';
+// import 'package:gpp/src/models/funcionalitie_model.dart';
+// import 'package:gpp/src/shared/repositories/status_code.dart';
+// import 'package:gpp/src/shared/services/gpp_api.dart';
+// import 'package:http/http.dart';
 
-class FuncionalitiesRepository {
-  Future<List<FuncionalitieModel>> fetchFuncionalities(UserModel user) async {
-    try {
-      Response response =
-          await gppApi.get('/user/funcionalidades/' + user.uid!);
+// class FuncionalitiesRepository {
+//   ApiService api;
 
-      if (response.statusCode == StatusCode.NOT_FOUND) {
-        var error = jsonDecode(response.body)['error'];
-        throw FuncionalitiesException(error);
-      }
+//   FuncionalitiesRepository({
+//     required this.api,
+//   });
 
-      var data = jsonDecode(response.body);
+//   Future<List<FuncionalitieModel>> fetchFuncionalities(UserModel user) async {
+//     try {
+//       Response response = await api.get('/user/funcionalidades/' + user.uid!);
 
-      List<FuncionalitieModel> funcionalidades = data.first
-          .map<FuncionalitieModel>((data) => FuncionalitieModel.fromJson(data))
-          .toList();
+//       if (response.statusCode == StatusCode.NOT_FOUND) {
+//         var error = jsonDecode(response.body)['error'];
+//         throw FuncionalitiesException(error);
+//       }
 
-      return funcionalidades;
-    } on FuncionalitiesException {
-      rethrow;
-    }
-  }
-}
+//       var data = jsonDecode(response.body);
+
+//       List<FuncionalitieModel> funcionalidades = data.first
+//           .map<FuncionalitieModel>((data) => FuncionalitieModel.fromJson(data))
+//           .toList();
+
+//       return funcionalidades;
+//     } on FuncionalitiesException {
+//       rethrow;
+//     }
+//   }
+// }
