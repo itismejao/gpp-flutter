ceca1acfaf5e3ed86e2c06a7205969db2af6090c
1032445 <weverson.lima@novomundo.com.br>
2021-12-30 17:29:18 -0300

    Tarefa: alterado controlador de funcionalidades

--- lib/src/controllers/funcionalities_controller.dart
+++ lib/src/controllers/funcionalities_controller.dart
@@ -1,44 +1,44 @@
-import 'package:flutter/cupertino.dart';
-import 'package:gpp/src/models/funcionalitie_model.dart';
-import 'package:gpp/src/models/user_model.dart';
-import 'package:gpp/src/repositories/funcionalities_repository.dart';
-import 'package:gpp/src/shared/enumeration/funcionalities_enum.dart';
-import 'package:gpp/src/shared/repositories/global.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:gpp/src/models/funcionalitie_model.dart';
+// import 'package:gpp/src/models/user_model.dart';
+// import 'package:gpp/src/repositories/funcionalities_repository.dart';
+// import 'package:gpp/src/shared/enumeration/funcionalities_enum.dart';
+// import 'package:gpp/src/shared/repositories/global.dart';
 
-class FuncionalitiesController {
-  late final FuncionalitiesRepository repository;
-  //FuncionalitiesEnum status = FuncionalitiesEnum.loading;
-  final state =
-      ValueNotifier<FuncionalitiesEnum>(FuncionalitiesEnum.notFuncionalities);
-  List<FuncionalitieModel> funcionalities = [];
-  List<FuncionalitieModel> funcionalitiesSearch = [];
+// class FuncionalitiesController {
+//   late final FuncionalitiesRepository repository;
+//   //FuncionalitiesEnum status = FuncionalitiesEnum.loading;
+//   final state =
+//       ValueNotifier<FuncionalitiesEnum>(FuncionalitiesEnum.notFuncionalities);
+//   List<FuncionalitieModel> funcionalities = [];
+//   List<FuncionalitieModel> funcionalitiesSearch = [];
 
-  FuncionalitiesController(this.repository);
+//   FuncionalitiesController(this.repository);
 
-  UserModel user = UserModel();
+//   UserModel user = UserModel();
 
-  void search(String value) {
-    state.value = FuncionalitiesEnum.loading;
-    if (value.isNotEmpty) {
-      funcionalitiesSearch = funcionalities
-          .where(
-              (row) => (row.name.toLowerCase().contains(value.toLowerCase())))
-          .toList();
+//   void search(String value) {
+//     state.value = FuncionalitiesEnum.loading;
+//     if (value.isNotEmpty) {
+//       funcionalitiesSearch = funcionalities
+//           .where(
+//               (row) => (row.name.toLowerCase().contains(value.toLowerCase())))
+//           .toList();
 
-      // funcionalitiesSearch.forEach((element) {
-      //   element.isExpanded = true;
-      // });
-    } else {
-      funcionalitiesSearch = [];
-    }
-    state.value = FuncionalitiesEnum.changeFuncionalities;
-  }
+//       // funcionalitiesSearch.forEach((element) {
+//       //   element.isExpanded = true;
+//       // });
+//     } else {
+//       funcionalitiesSearch = [];
+//     }
+//     state.value = FuncionalitiesEnum.changeFuncionalities;
+//   }
 
-  Future<void> changeFuncionalities() async {
-    user.uid = authenticateUser!.id.toString();
-    state.value = FuncionalitiesEnum.loading;
-    funcionalities = await repository.fetchFuncionalities(user);
-    //funcionalitiesAux = funcionalities;
-    state.value = FuncionalitiesEnum.changeFuncionalities;
-  }
-}
+//   Future<void> changeFuncionalities() async {
+//     user.uid = authenticateUser!.id.toString();
+//     state.value = FuncionalitiesEnum.loading;
+//     funcionalities = await repository.fetchFuncionalities(user);
+//     //funcionalitiesAux = funcionalities;
+//     state.value = FuncionalitiesEnum.changeFuncionalities;
+//   }
+// }
