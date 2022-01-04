c8f064d49421e35c66cd0fd6935efb354a6a0e6c
1032445 <weverson.lima@novomundo.com.br>
2022-01-04 18:02:53 -0300

    Tarefa: criado repositório de funcionalidades

--- lib/src/repositories/funcionalities_repository.dart
+++ lib/src/repositories/funcionalities_repository.dart
@@ -1,37 +1,67 @@
-// import 'dart:convert';
+import 'dart:convert';
 
-// import 'package:gpp/src/models/user_model.dart';
-// import 'package:gpp/src/shared/exceptions/funcionalities_exception.dart';
-// import 'package:gpp/src/models/funcionalitie_model.dart';
-// import 'package:gpp/src/shared/repositories/status_code.dart';
-// import 'package:gpp/src/shared/services/gpp_api.dart';
-// import 'package:http/http.dart';
+import 'package:gpp/src/models/user_model.dart';
+import 'package:gpp/src/shared/exceptions/funcionalities_exception.dart';
+import 'package:gpp/src/models/funcionalitie_model.dart';
+import 'package:gpp/src/shared/repositories/status_code.dart';
+import 'package:gpp/src/shared/services/gpp_api.dart';
+import 'package:http/http.dart';
 
-// class FuncionalitiesRepository {
-//   ApiService api;
+class FuncionalitiesRepository {
+  ApiService api;
 
-//   FuncionalitiesRepository({
-//     required this.api,
-//   });
+  FuncionalitiesRepository({
+    required this.api,
+  });
 
-//   Future<List<FuncionalitieModel>> fetchFuncionalities(UserModel user) async {
-//     try {
-//       Response response = await api.get('/user/funcionalidades/' + user.uid!);
+  Future<List<FuncionalitieModel>> fetch() async {
+    Response response = await api.get('/funcionalidades');
 
-//       if (response.statusCode == StatusCode.NOT_FOUND) {
-//         var error = jsonDecode(response.body)['error'];
-//         throw FuncionalitiesException(error);
-//       }
+    if (response.statusCode == StatusCode.OK) {
+      var data = jsonDecode(response.body);
 
-//       var data = jsonDecode(response.body);
+      List<FuncionalitieModel> funcionalidades = data.first
+          .map<FuncionalitieModel>((data) => FuncionalitieModel.fromJson(data))
+          .toList();
 
-//       List<FuncionalitieModel> funcionalidades = data.first
-//           .map<FuncionalitieModel>((data) => FuncionalitieModel.fromJson(data))
-//           .toList();
+      return funcionalidades;
+    } else {
+      throw FuncionalitiesException("Funcionalidades não encontrada !");
+    }
+  }
 
-//       return funcionalidades;
-//     } on FuncionalitiesException {
-//       rethrow;
-//     }
-//   }
-// }
+  Future<bool> create(FuncionalitieModel funcionalitie) async {
+    Response response =
+        await api.post('/funcionalidades', funcionalitie.toJson());
+
+    if (response.statusCode == StatusCode.OK) {
+      return true;
+    } else {
+      throw FuncionalitiesException("Funcionalidade não foi cadastrada !");
+    }
+  }
+
+  Future<bool> update(FuncionalitieModel funcionalitie) async {
+    Response response = await api.put(
+        '/funcionalidades/' + funcionalitie.id.toString(),
+        funcionalitie.toJson());
+
+    if (response.statusCode == StatusCode.OK) {
+      return true;
+    } else {
+      throw FuncionalitiesException("Funcionalidade não foi atualizada !");
+    }
+  }
+
+  Future<bool> delete(FuncionalitieModel funcionalitie) async {
+    Response response = await api.delete(
+      '/funcionalidades/' + funcionalitie.id.toString(),
+    );
+
+    if (response.statusCode == StatusCode.OK) {
+      return true;
+    } else {
+      throw FuncionalitiesException("Funcionalidade não foi deletada !");
+    }
+  }
+}
