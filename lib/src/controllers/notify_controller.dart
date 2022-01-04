8ea12798a833fb5de6164dafe9136a92e5538877
1032445 <weverson.lima@novomundo.com.br>
2022-01-04 18:01:28 -0300

    Tarefa: implementado método de alerta

--- lib/src/controllers/notify_controller.dart
+++ lib/src/controllers/notify_controller.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 
 class NotifyController {
   BuildContext context;
-  String message;
+
   NotifyController({
     required this.context,
-    required this.message,
   });
 
-  void error() {
+  void error(String message) {
     final snackBar = SnackBar(
         duration: const Duration(seconds: 5),
         backgroundColor: Colors.red,
@@ -16,11 +15,39 @@ class NotifyController {
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
-  void sucess() {
+  void sucess(String message) {
     final snackBar = SnackBar(
         duration: const Duration(seconds: 5),
         backgroundColor: Colors.green,
         content: Text(message));
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  Future<bool> alert(String message) async {
+    return await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Confirmação'),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context, rootNavigator: true)
+                    .pop(false); // dismisses only the dialog and returns false
+              },
+              child: Text('Não'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context, rootNavigator: true)
+                    .pop(true); // dismisses only the dialog and returns true
+              },
+              child: Text('Sim'),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
