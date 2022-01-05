5f82ee7ce44a6ecafbcbe7e9d7007013e3b33696
1032445 <weverson.lima@novomundo.com.br>
2022-01-05 17:54:46 -0300

    Tarefa: criado controlador de subfuncionalidades

---
+++ lib/src/controllers/subfuncionalities_controller.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/cupertino.dart';
+import 'package:gpp/src/models/funcionalitie_model.dart';
+import 'package:gpp/src/models/subfuncionalities_model.dart';
+import 'package:gpp/src/models/user_model.dart';
+import 'package:gpp/src/repositories/funcionalities_repository.dart';
+import 'package:gpp/src/repositories/subfuncionalities_repository.dart';
+import 'package:gpp/src/shared/enumeration/funcionalities_enum.dart';
+import 'package:gpp/src/shared/enumeration/subfuncionalities_enum.dart';
+import 'package:gpp/src/shared/repositories/global.dart';
+
+class SubFuncionalitiesController {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  late final SubFuncionalitiesRepository repository;
+
+  List<SubFuncionalitiesModel> subFuncionalities = [];
+
+  SubFuncionalitiesModel subFuncionalitie = SubFuncionalitiesModel();
+  SubFuncionalitiesEnum state = SubFuncionalitiesEnum.notChange;
+  // bool groupValue = false;
+
+  SubFuncionalitiesController({required this.repository});
+
+  Future<void> fetch(FuncionalitieModel funcionalitie) async {
+    subFuncionalities = await repository.fetch(funcionalitie);
+  }
+
+  void setSubFuncionalitieName(value) {
+    subFuncionalitie.name = value;
+  }
+
+  void setSubFuncionalitieIcon(value) {
+    subFuncionalitie.icon = value;
+  }
+
+  void setSubFuncionalitieRoute(value) {
+    subFuncionalitie.route = value;
+  }
+
+  void setSubFuncionalitieActive(value) {
+    subFuncionalitie.active = value;
+  }
+
+  // //Validação
+  validateInput(value) {
+    if (value.isEmpty) {
+      return 'Campo obrigatório';
+    }
+    return null;
+  }
+
+  Future<bool> create(FuncionalitieModel funcionalitie) async {
+    if (!formKey.currentState!.validate()) {
+      return false;
+    }
+
+    formKey.currentState!.save();
+
+    return await repository.create(funcionalitie, subFuncionalitie);
+  }
+
+  // Future<bool> update(FuncionalitieModel funcionalitie) async {
+  //   return await repository.update(funcionalitie);
+  // }
+
+  Future<bool> delete(SubFuncionalitiesModel subFuncionalitie) async {
+    return await repository.delete(subFuncionalitie);
+  }
+}
