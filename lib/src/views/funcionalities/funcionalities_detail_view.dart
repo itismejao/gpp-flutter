c35c1c6e75f62691b9fcf913dfac9767a3552fd1
1032445 <weverson.lima@novomundo.com.br>
2022-01-05 17:58:45 -0300

    Tarefa: criado visão funcionalidade detalhe

---
+++ lib/src/views/funcionalities/funcionalities_detail_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:gpp/src/models/funcionalitie_model.dart';
+import 'package:gpp/src/views/subfuncionalities/subfuncionalities_home_view.dart';
+import 'package:gpp/src/views/subfuncionalities/subfuncionalities_list_view.dart';
+
+class FuncionalitiesDetailView extends StatefulWidget {
+  FuncionalitieModel funcionalitie;
+  FuncionalitiesDetailView({Key? key, required this.funcionalitie})
+      : super(key: key);
+
+  @override
+  State<FuncionalitiesDetailView> createState() =>
+      _FuncionalitiesDetailViewState();
+}
+
+class _FuncionalitiesDetailViewState extends State<FuncionalitiesDetailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SubFuncionalitiesHomeView(
+        funcionalitie: widget.funcionalitie,
+      ),
+    );
+  }
+}
