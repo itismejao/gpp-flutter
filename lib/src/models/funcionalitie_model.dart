68643c42117dc22599932db0a6e61d9dbb07a696
1032445 <weverson.lima@novomundo.com.br>
2022-01-05 17:55:14 -0300

    Tarefa: alterado modelo de funcionalidade

--- lib/src/models/funcionalitie_model.dart
+++ lib/src/models/funcionalitie_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gpp/src/models/subfuncionalities_model.dart';
 
 class FuncionalitieModel {
   int? id;
@@ -6,7 +7,7 @@ class FuncionalitieModel {
   String? icon;
   bool? active;
 
-  List<SubFuncionalities>? subFuncionalities;
+  List<SubFuncionalitiesModel>? subFuncionalities;
   bool isExpanded = false;
   FuncionalitieModel({
     this.id,
@@ -24,10 +25,10 @@ class FuncionalitieModel {
     if (json['active'] != null) {
       active = int.parse(json['active']) == 1 ? true : false;
     }
-    List<SubFuncionalities> subFuncionalities = [];
+    List<SubFuncionalitiesModel> subFuncionalities = [];
     if (json['subFuncionalities'] != null) {
       json['subFuncionalities'].forEach((data) {
-        subFuncionalities.add(SubFuncionalities.fromJson(data));
+        subFuncionalities.add(SubFuncionalitiesModel.fromJson(data));
       });
     }
     return FuncionalitieModel(
@@ -48,42 +49,6 @@ class FuncionalitieModel {
         ? subFuncionalities!.map((v) => v.toJson()).toList()
         : null;
 
-    return data;
-  }
-}
-
-class SubFuncionalities {
-  String id;
-  String? name;
-  String? icon;
-  String? route;
-  int? active;
-  String? idRegister;
-  Color? colorButton;
-  BoxDecoration? boxDecoration;
-  // Border? border =
-  //     Border(left: BorderSide(color: Colors.grey.shade200, width: 2));
-
-  SubFuncionalities(this.id,
-      {this.name, this.active, this.icon, this.route, this.idRegister});
-
-  factory SubFuncionalities.fromJson(Map<String, dynamic> json) {
-    return SubFuncionalities(json['id'],
-        name: json['name'],
-        active: json['active'],
-        icon: json['icon'],
-        route: json['route'],
-        idRegister: json['idregister']);
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['id'] = id;
-    data['name'] = name;
-    data['active'] = active;
-    data['icon'] = icon;
-    data['route'] = route;
-    data['idregister'] = idRegister;
     return data;
   }
 }
