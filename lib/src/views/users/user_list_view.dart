dcf484ad6ea8f7021d251919dd6851da62ebbe96
1032445 <weverson.lima@novomundo.com.br>
2022-01-05 18:00:57 -0300

    Tarefa; alterado visão de lista de usuário

---
+++ lib/src/views/users/user_list_view.dart
@@ -0,0 +1,424 @@
+import 'package:flutter/material.dart';
+import 'package:gpp/src/controllers/responsive_controller.dart';
+import 'package:gpp/src/controllers/user_controller.dart';
+import 'package:gpp/src/models/user_model.dart';
+import 'package:gpp/src/repositories/user_repository.dart';
+import 'package:gpp/src/shared/enumeration/user_enum.dart';
+import 'package:gpp/src/shared/repositories/styles.dart';
+import 'package:gpp/src/shared/services/gpp_api.dart';
+import 'package:gpp/src/views/loading_view.dart';
+
+class UserListView extends StatefulWidget {
+  const UserListView({Key? key}) : super(key: key);
+
+  @override
+  _UserListViewState createState() => _UserListViewState();
+}
+
+class _UserListViewState extends State<UserListView> {
+  late final UserController _controller =
+      UserController(repository: UserRepository(api: gppApi));
+  final ResponsiveController _responsive = ResponsiveController();
+  void changeUsers() async {
+    if (mounted) {
+      setState(() {
+        _controller.state = UserEnum.loading;
+      });
+    }
+    await _controller.changeUser();
+    if (mounted) {
+      setState(() {
+        _controller.state = UserEnum.changeUser;
+      });
+    }
+  }
+
+  @override
+  initState() {
+    // ignore: todo
+    // TODO: implement initState
+    super.initState();
+
+    changeUsers();
+  }
+
+  DropdownButtonFormField<String> dropDownButton() {
+    return DropdownButtonFormField<String>(
+        decoration: inputDecoration(
+          'Selecione o departamento',
+          const Icon(
+            Icons.format_list_bulleted,
+          ),
+        ),
+        // value: dropdownValue,
+        icon: const Icon(Icons.arrow_downward),
+        onChanged: (value) {
+          if (mounted) {
+            setState(() {
+              _controller.state = UserEnum.loading;
+            });
+          }
+          _controller.search(value!);
+          if (mounted) {
+            setState(() {
+              _controller.state = UserEnum.changeUser;
+            });
+          }
+        },
+        items: <String>['One', 'Two', 'Free', 'Four']
+            .map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value),
+          );
+        }).toList(),
+        hint: const Text(
+          "Selecione o departamento",
+          style: TextStyle(
+              color: Colors.black, fontSize: 14, fontWeight: FontWeight.w500),
+        ));
+  }
+
+  TextFormField inputSearch() {
+    return TextFormField(
+      onChanged: (value) {
+        if (mounted) {
+          setState(() {
+            _controller.state = UserEnum.loading;
+          });
+        }
+        _controller.search(value);
+        if (mounted) {
+          setState(() {
+            _controller.state = UserEnum.changeUser;
+          });
+        }
+      },
+      style: textStyle(
+          fontWeight: FontWeight.w700,
+          color: Colors.black,
+          fontSize: 14,
+          height: 1.8),
+      decoration: inputDecoration('Buscar', const Icon(Icons.search)),
+    );
+  }
+
+  stateManager() {
+    switch (_controller.state) {
+      case UserEnum.loading:
+        return const LoadingView();
+
+      case UserEnum.changeUser:
+        return _controller.usersSearch.isEmpty
+            ? _buildUserList(_controller.users)
+            : _buildUserList(_controller.usersSearch);
+      case UserEnum.notUser:
+        // ignore: todo
+        // TODO: Handle this case.
+        break;
+    }
+  }
+
+  Widget _buildUserList(List<UserModel> users) {
+    Widget widget = LayoutBuilder(
+      builder: (context, constraints) {
+        if (_responsive.isMobile(constraints.maxWidth)) {
+          return ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) {
+                return _buildListItem(users, index, context);
+              });
+        }
+
+        return Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                      child: Text('Foto',
+                          style: textStyle(
+                              color: Colors.grey.shade400,
+                              fontWeight: FontWeight.w700))),
+                  Expanded(
+                    flex: 2,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text('Nome',
+                            style: textStyle(
+                                color: Colors.grey.shade400,
+                                fontWeight: FontWeight.w700)),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                      child: Text('RE',
+                          style: textStyle(
+                              color: Colors.grey.shade400,
+                              fontWeight: FontWeight.w700))),
+                  Expanded(
+                      flex: 2,
+                      child: Text('E-mail',
+                          style: textStyle(
+                              color: Colors.grey.shade400,
+                              fontWeight: FontWeight.w700))),
+                  Expanded(
+                      child: Text('Departamento',
+                          style: textStyle(
+                              color: Colors.grey.shade400,
+                              fontWeight: FontWeight.w700))),
+                  Expanded(
+                    child: Center(
+                        child: Text('Status',
+                            style: textStyle(
+                                color: Colors.grey.shade400,
+                                fontWeight: FontWeight.w700))),
+                  ),
+                  Expanded(
+                    child: Center(
+                        child: Text('Ação',
+                            style: textStyle(
+                                color: Colors.grey.shade400,
+                                fontWeight: FontWeight.w700))),
+                  )
+                ],
+              ),
+            ),
+            const Divider(),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: users.length,
+                  itemBuilder: (context, index) {
+                    return _buildListItem(users, index, context);
+                  }),
+            )
+          ],
+        );
+      },
+    );
+
+    return Container(color: Colors.white, child: widget);
+  }
+
+  Widget _buildListItem(
+      List<UserModel> users, int index, BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (_responsive.isMobile(constraints.maxWidth)) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  border: Border(
+                      left: BorderSide(
+                          color: users[index].active == "1"
+                              ? secundaryColor
+                              : Colors.grey.shade400,
+                          width: 4))),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                            height: 50,
+                            width: 50,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network(
+                                  'https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png'),
+                            )),
+                        const SizedBox(
+                          width: 12,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              users[index].name!,
+                              style: textStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                            const SizedBox(
+                              height: 6,
+                            ),
+                            users[index].departement != null
+                                ? Text(
+                                    users[index].departement!,
+                                    style: textStyle(
+                                        color: Colors.grey.shade400,
+                                        fontWeight: FontWeight.w700),
+                                  )
+                                : Text('',
+                                    style: textStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.w700))
+                          ],
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(
+                            Icons.edit,
+                            color: Colors.grey.shade400,
+                          ),
+                          onPressed: () => {
+                            Navigator.pushNamed(context, '/user_detail',
+                                arguments: users[index])
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(
+                            Icons.delete,
+                            color: Colors.grey.shade400,
+                          ),
+                          onPressed: () => {
+                            // Navigator.pushNamed(context, '/user_detail',
+                            //     arguments: users[index])
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        }
+
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                        height: 50,
+                        width: 50,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network(
+                              'https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png'),
+                        )),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Text(
+                  users[index].name!,
+                  style: textStyle(
+                      color: Colors.black, fontWeight: FontWeight.w700),
+                ),
+              ),
+              Expanded(
+                  child: Text(
+                users[index].uid!,
+                style:
+                    textStyle(color: Colors.black, fontWeight: FontWeight.w700),
+              )),
+              Expanded(
+                  flex: 2,
+                  child: Text(
+                    users[index].email!,
+                    style: textStyle(
+                        color: Colors.black, fontWeight: FontWeight.w700),
+                  )),
+              Expanded(
+                  child: users[index].departement != null
+                      ? Text(
+                          users[index].departement!,
+                          style: textStyle(
+                              color: Colors.black, fontWeight: FontWeight.w700),
+                        )
+                      : Text('',
+                          style: textStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w700))),
+              Expanded(
+                  child: Container(
+                height: 10,
+                width: 10,
+                decoration: BoxDecoration(
+                    color: users[index].active == "1"
+                        ? secundaryColor
+                        : Colors.grey.shade400,
+                    shape: BoxShape.circle),
+              )),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: Icon(
+                        Icons.edit,
+                        color: Colors.grey.shade400,
+                      ),
+                      onPressed: () => {
+                        Navigator.pushNamed(context, '/user_detail',
+                            arguments: users[index])
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.delete,
+                        color: Colors.grey.shade400,
+                      ),
+                      onPressed: () => {
+                        // Navigator.pushNamed(context, '/user_detail',
+                        //     arguments: users[index])
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12.0),
+            child: Row(
+              children: [
+                Expanded(
+                    child: Text('Usuários',
+                        style: textStyle(
+                            fontSize: 24,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w700))),
+              ],
+            ),
+          ),
+          //  _buildFilterUsers(),
+          Expanded(
+            child: stateManager(),
+          )
+        ],
+      ),
+    );
+  }
+}
