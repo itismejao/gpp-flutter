e99bd9e54f91d6e42865c84fa02ce3b5502b97d2
1032445 <weverson.lima@novomundo.com.br>
2022-01-06 18:11:59 -0300

    Tarefa: alterado repositório de autenticação

--- lib/src/repositories/authenticate_repository.dart
+++ lib/src/repositories/authenticate_repository.dart
@@ -4,7 +4,6 @@ import 'package:gpp/src/models/authenticate_model.dart';
 import 'package:gpp/src/models/user_model.dart';
 import 'package:gpp/src/shared/exceptions/authenticate_exception.dart';
 
-import 'package:gpp/src/shared/models/authenticate_model.dart';
 import 'package:gpp/src/shared/repositories/global.dart';
 import 'package:gpp/src/shared/repositories/status_code.dart';
 import 'package:gpp/src/shared/services/auth.dart';
