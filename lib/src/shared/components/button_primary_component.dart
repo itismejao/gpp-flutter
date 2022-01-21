990bbe633a499c45577fb4e1a84dd3bcd94a00b9
1032445 <weverson.lima@novomundo.com.br>
2022-01-21 18:17:08 -0300

    Tarefa: alterado componente de botão primário

--- lib/src/shared/components/button_primary_component.dart
+++ lib/src/shared/components/button_primary_component.dart
@@ -1,29 +1,43 @@
 import 'package:flutter/material.dart';
+
 import 'package:gpp/src/shared/repositories/styles.dart';
 
 class ButtonPrimaryComponent extends StatelessWidget {
   final Function onPressed;
   final String text;
+  final Color? color;
+  Icon? icon;
 
-  const ButtonPrimaryComponent({
+  ButtonPrimaryComponent({
     Key? key,
     required this.onPressed,
     required this.text,
+    this.color,
+    this.icon,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-        style: ElevatedButton.styleFrom(
-            shadowColor: Colors.transparent,
-            elevation: 0,
-            primary: primaryColor),
-        onPressed: () => onPressed(),
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Text(text,
-              style:
-                  textStyle(color: Colors.white, fontWeight: FontWeight.w700)),
-        ));
+    return GestureDetector(
+      onTap: () => onPressed(),
+      child: Container(
+        height: 40,
+        width: 80,
+        decoration:
+            BoxDecoration(color: color, borderRadius: BorderRadius.circular(5)),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(text,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12,
+                    letterSpacing: 0.15)),
+          ],
+        ),
+      ),
+    );
   }
 }
