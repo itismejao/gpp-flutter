13d292d2941b53ff34e0ccb4c2b12d058db7b5c4
1032445 <weverson.lima@novomundo.com.br>
2022-01-25 15:40:10 -0300

    Tarefa: refatoração

--- lib/src/shared/repositories/routes.dart
+++
@@ -1,8 +0,0 @@
-class Routes {
-  // ignore: constant_identifier_names
-  static const String LOGIN = '/login';
-  // ignore: constant_identifier_names
-  static const String HOME = '/home';
-  // ignore: constant_identifier_names
-  static const String NOT_FOUND = '/not_found';
-}
