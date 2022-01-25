03204ddc4ff58c4f3c41868eb73a864e9dda9262
1032445 <weverson.lima@novomundo.com.br>
2022-01-25 15:41:48 -0300

    Tarefa: criado componente dropdown

--- lib/src/shared/components/drop_down_component.dart
+++ lib/src/shared/components/drop_down_component.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:gpp/src/shared/components/text_component.dart';
 
 class DropDownComponent extends StatelessWidget {
-  Icon? icon;
-  String? hintText;
-  List<DropdownMenuItem<String>> items;
-  DropDownComponent({
+  final String? label;
+  final Icon? icon;
+  final String? hintText;
+  final List<DropdownMenuItem<String>> items;
+  const DropDownComponent({
     Key? key,
+    this.label,
     this.icon,
     this.hintText,
     required this.items,
@@ -13,22 +16,45 @@ class DropDownComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: Colors.grey.shade200, borderRadius: BorderRadius.circular(5)),
-      child: DropdownButtonFormField<String>(
-        isExpanded: true,
-        style: TextStyle(fontSize: 12),
-        decoration: InputDecoration(
-            contentPadding:
-                EdgeInsets.only(top: 10, bottom: 10, left: 10, right: 10),
-            hintText: hintText,
-            prefixIcon: icon,
-            hintStyle: TextStyle(fontSize: 12, height: 2),
-            border: InputBorder.none),
-        items: items.toList(),
-        onChanged: (_) {},
-      ),
+    final media = MediaQuery.of(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        label != null ? TextComponent(label!) : Container(),
+        label != null
+            ? SizedBox(
+                height: 8,
+              )
+            : Container(),
+        Container(
+          decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.circular(5)),
+          child: DropdownButtonFormField<String>(
+            isExpanded: true,
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 16 * media.textScaleFactor,
+                letterSpacing: 0.15,
+                fontStyle: FontStyle.normal,
+                fontWeight: FontWeight.w500),
+            decoration: InputDecoration(
+                contentPadding:
+                    EdgeInsets.only(top: 15, bottom: 10, left: 10, right: 10),
+                hintText: hintText,
+                prefixIcon: icon,
+                hintStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16 * media.textScaleFactor,
+                    letterSpacing: 0.15,
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w500),
+                border: InputBorder.none),
+            items: items.toList(),
+            onChanged: (_) {},
+          ),
+        ),
+      ],
     );
   }
 }
