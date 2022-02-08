4f13453ad0f649a55dbc8458861307dd6f6e4420
1033295 <mateus.luiz@novomundo.com.br>
2022-02-08 13:11:38 -0300

    Começo da criação da View

--- lib/main.dart
+++ lib/main.dart
@@ -106,11 +106,11 @@ class _GppAppState extends State<GppApp> {
           inputDecorationTheme: const InputDecorationTheme(iconColor: Colors.grey)),
       onGenerateRoute: (settings) {
         //Teste
-        // return MaterialPageRoute(
-        //     builder: (context) => HomeView(
-        //           funcionalities: FuncionalitiesView(),
-        //           page: AstecaListView(),
-        //         ));
+        return MaterialPageRoute(
+            builder: (context) => HomeView(
+                  funcionalities: FuncionalitiesView(),
+                  page: PecasDetailView(),
+                ));
 
         // Handle '/'
         //return MaterialPageRoute(builder: (context) => Scaffold(body: AstecaListView()));
--- lib/src/views/pecas/pecas_detail_view.dart
+++ lib/src/views/pecas/pecas_detail_view.dart
@@ -1,4 +1,9 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:gpp/src/shared/components/title_component.dart';
+import 'package:gpp/src/views/asteca/components/item_menu.dart';
+import 'package:gpp/src/shared/repositories/styles.dart';
 
 class PecasDetailView extends StatefulWidget {
   const PecasDetailView({Key? key}) : super(key: key);
@@ -8,10 +13,100 @@ class PecasDetailView extends StatefulWidget {
 }
 
 class _PecasDetailViewState extends State<PecasDetailView> {
+  int selected = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Cadastrar Peças'),
+    return Row(
+      children: [
+        Expanded(
+          child: _pecasMenu(),
+        ),
+        Expanded(
+          flex: 4,
+          child: Column(
+            children: [
+              _pecasNavigator(),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  _pecasMenu() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: const TitleComponent(
+            'Cadastros',
+          ),
+        ),
+        Divider(),
+        Row(
+          children: [
+            Expanded(
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selected = 1;
+                  });
+                },
+                child: ItemMenu(
+                  color: selected == 1 ? Colors.grey.shade50 : Colors.transparent,
+                  borderColor: selected == 1 ? secundaryColor : Colors.transparent,
+                  data: 'Peça',
+                ),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selected = 2;
+                  });
+                },
+                child: ItemMenu(
+                  color: selected == 2 ? Colors.grey.shade50 : Colors.transparent,
+                  borderColor: selected == 2 ? secundaryColor : Colors.transparent,
+                  data: 'Cores',
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  _pecasNavigator() {
+    switch (selected) {
+      case 1:
+        return _selectedPeca();
+      case 2:
+        return _selectedCores();
+    }
+  }
+
+  _selectedPeca() {
+    return Column(
+      children: [
+        Text('Aba Peça'),
+      ],
+    );
+  }
+
+  _selectedCores() {
+    return Column(
+      children: [
+        Text('Aba Cores'),
+      ],
     );
   }
 }
