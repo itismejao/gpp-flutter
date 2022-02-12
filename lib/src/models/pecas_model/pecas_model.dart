e8e64b1407c6627beace51bf44e6ea94b8d7fe42
1033295 <mateus.luiz@novomundo.com.br>
2022-02-12 10:52:18 -0300

    Atualizacao model pecas

--- lib/src/models/pecas_model/pecas_model.dart
+++ lib/src/models/pecas_model/pecas_model.dart
@@ -6,71 +6,74 @@ class PecasModel {
   // int? id_produto;
   // int? id_fornecedor;
   // Fim teste
-  int? id_peca;
+  int? id_peca_especie;
   String? numero;
   String? codigo_fabrica;
-  double? custo;
   int? unidade;
   String? descricao;
-  double? largura;
   double? altura;
+  double? largura;
   double? profundidade;
-  double? volume;
   int? unidade_medida;
+  double? volumes;
+  int? active;
+  double? custo;
   int? classificacao_custo;
   int? tipo_classificacao_custo;
-  int? active;
-  int? id_peca_especie;
-  int? quantidade;
-  int? id_cor;
   int? id_peca_material_fabricacao;
-  int? id_produto;
+  int? id_peca_cor;
+  DateTime? created_at;
+  DateTime? updated_at;
+  String? material_fabricacao;
+  String? especie;
 
   // List<ProdutoPecaModel>? produto_peca;
 
   PecasModel({
-    this.id_peca,
+    this.id_peca_especie,
     this.numero,
     this.codigo_fabrica,
-    this.custo,
     this.unidade,
     this.descricao,
-    this.largura,
     this.altura,
+    this.largura,
     this.profundidade,
-    this.volume,
     this.unidade_medida,
+    this.volumes,
+    this.active,
+    this.custo,
     this.classificacao_custo,
     this.tipo_classificacao_custo,
-    this.active,
-    this.id_peca_especie,
-    this.quantidade,
-    this.id_cor,
     this.id_peca_material_fabricacao,
-    this.id_produto,
+    this.id_peca_cor,
+    this.created_at,
+    this.updated_at,
+    this.material_fabricacao,
+    this.especie,
   });
 
   factory PecasModel.fromJson(Map<String, dynamic> json) {
     return PecasModel(
-      id_peca: json['id_peca'],
+      id_peca_especie: json['id_peca_especie'],
       numero: json['numero'],
       codigo_fabrica: json['codigo_fabrica'],
-      custo: json['custo'],
       unidade: json['unidade'],
       descricao: json['descricao'],
-      largura: json['largura'],
       altura: json['altura'],
+      largura: json['largura'],
       profundidade: json['profundidade'],
-      volume: json['volume'],
       unidade_medida: json['unidade_medida'],
+      volumes: json['volumes'],
+      active: json['active'],
+      custo: json['custo'],
       classificacao_custo: json['classificacao_custo'],
       tipo_classificacao_custo: json['tipo_classificacao_custo'],
-      active: json['active'],
-      id_peca_especie: json['id_peca_especie'],
-      quantidade: json['quantidade'],
-      id_cor: json['id_cor'],
       id_peca_material_fabricacao: json['id_peca_material_fabricacao'],
-      id_produto: json['id_produto'],
+      id_peca_cor: json['id_peca_cor'],
+      created_at: json['created_at'],
+      updated_at: json['updated_at'],
+      material_fabricacao: json['material_fabricacao'],
+      especie: json['especie'],
       // produto_peca: json['produto_peca'] != null
       //     ? json['produto_peca'].map<ProdutoPecaModel>((data) {
       //         return ProdutoPecaModel.fromJson(data);
@@ -82,25 +85,26 @@ class PecasModel {
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
 
-    data['id_peca'] = this.id_peca;
+    data['id_peca_especie'] = this.id_peca_especie;
     data['numero'] = this.numero;
     data['codigo_fabrica'] = this.codigo_fabrica;
-    data['custo'] = this.custo;
     data['unidade'] = this.unidade;
     data['descricao'] = this.descricao;
-    data['largura'] = this.largura;
     data['altura'] = this.altura;
+    data['largura'] = this.largura;
     data['profundidade'] = this.profundidade;
-    data['volume'] = this.volume;
     data['unidade_medida'] = this.unidade_medida;
+    data['volumes'] = this.volumes;
+    data['active'] = this.active;
+    data['custo'] = this.custo;
     data['classificacao_custo'] = this.classificacao_custo;
     data['tipo_classificacao_custo'] = this.tipo_classificacao_custo;
-    data['active'] = this.active;
-    data['id_peca_especie'] = this.id_peca_especie;
-    data['quantidade'] = this.quantidade;
-    data['id_cor'] = this.id_cor;
     data['id_peca_material_fabricacao'] = this.id_peca_material_fabricacao;
-    data['id_produto'] = this.id_produto;
+    data['id_peca_cor'] = this.id_peca_cor;
+    data['created_at'] = this.created_at;
+    data['updated_at'] = this.updated_at;
+    data['material_fabricacao'] = this.material_fabricacao;
+    data['especie'] = this.especie;
 
     // if (this.produto_peca != null) {
     //   data['produto_peca'] = this.produto_peca!.map((value) => value.toJson()).toList();
