519e39d52a586d73c96e3c1c37bfa8c880e615bf
1032445 <weverson.lima@novomundo.com.br>
2022-02-19 12:12:02 -0300

    Tarefa: refatorado modelo de peça

--- lib/src/models/PecaModel.dart
+++ lib/src/models/PecaModel.dart
@@ -7,4 +7,8 @@ class PecaModel {
     this.descricao,
     required this.custo,
   });
+
+  factory PecaModel.fromJson(Map<String, dynamic> json) {
+    return PecaModel(idPeca: json['id_peca'], custo: json['custo']);
+  }
 }
