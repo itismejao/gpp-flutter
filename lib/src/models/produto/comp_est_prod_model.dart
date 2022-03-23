72047190f3fba698f72ff6103e9060939e16d894
1032445 <weverson.lima@novomundo.com.br>
2022-03-23 11:50:52 -0300

    Tarefa: criado modelo de comp_est_prod

---
+++ lib/src/models/produto/comp_est_prod_model.dart
@@ -0,0 +1,23 @@
+import 'package:gpp/src/models/produto_model.dart';
+
+class CompEstProd {
+  int? idCompEst;
+  ProdutoModel? produto;
+
+  CompEstProd({this.idCompEst, this.produto});
+
+  factory CompEstProd.fromJson(Map<String, dynamic> json) {
+    return CompEstProd(
+        idCompEst: json['id_comp_est'] != null ? json['id_comp_est'] : null,
+        produto: json['produto'] != null
+            ? ProdutoModel.fromJson(json['produto'])
+            : null);
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id_comp_est'] = this.idCompEst;
+    data['produto'] = this.produto != null ? this.produto!.toJson() : null;
+    return data;
+  }
+}
